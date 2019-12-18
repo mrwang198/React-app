ea24ffffaae31ab0e814fed2972cce73a9a7b476
wangxiaosen <2644767982@qq.com>
2019-12-18 15:15:30 +0800

    第一次提交

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/**
+ * 应用根组件
+ */
+import React ,{Component} from "react";
+class App extends Component{
+    render(){
+        return (
+            <div>App</div>
+        )
+    }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * webpack入口文件
+ * webpack打包的必要文件
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+ReactDOM.render(<App/>,document.getElementById('root'));
